748956266c1535323c8e719f68bd5088a8eaa838
uppadashajini-art <uppadashajini@gmail.com>
2026-09-15 20:40:17 +0530

    Fix Vercel CORS origin

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -13,6 +13,8 @@ import axios from "axios";
 const API_URL =
   import.meta.env.VITE_API_URL ||
   "http://localhost:8080/api";
+  console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("FINAL API_URL:", API_URL);
 
 // ===============================
 // AXIOS INSTANCE
